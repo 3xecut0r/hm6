a44a09763c6bf0cbfa5af7bbce3be79874456e42
Oleksii Panpukha <58779018+3xecut0r@users.noreply.github.com>
2023-05-24 14:02:24 +0200

    finally

--- query_num2.sql
+++ query_num2.sql
@@ -1,4 +1,4 @@
-SELECT s.fullname, d.name ROUND(AVG(g.grade),2) AS averange_grade
+SELECT s.fullname, d.name, ROUND(AVG(g.grade),2) AS averange_grade
 FROM grades g
 JOIN students s ON s.id = g.student_id
 JOIN disciplines d ON d.id = g.discipline_id
